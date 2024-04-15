03a5f9baab1e6280c3f75c13430ba9c79ef9f416
Bastien Winant <bastien.winant@gmail.com>
2024-04-15 23:26:55 +0200

    create line graph for sale volume

--- dashboard.py
+++ dashboard.py
@@ -1,53 +1,24 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-
-from dash import Dash, dcc, html
+from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
 
+df = pd.read_csv('processed_data.csv')
+
 app = Dash(__name__)
 
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
-
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Hello Dash',
-        style={
-            'textAlign': 'center',
-            'color': colors['text']
-        }
-    ),
-
-    html.Div(children='Dash: A web application framework for your data.', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure=fig
-    )
+app.layout = html.Div([
+    html.H1(children='Pink Morsel Sale Volume', style={'textAlign':'center'}),
+    dcc.Dropdown(df.region.unique(), 'north', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
 ])
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.region==value]
+    return px.line(dff, x='date', y='sales')
 
 if __name__ == '__main__':
     app.run(debug=True)
