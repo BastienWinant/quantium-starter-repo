28f46997d6a9d9f2acdd47e2f8b06cfbd365a5ba
Bastien Winant <bastien.winant@gmail.com>
2024-07-31 22:01:47 +0200

    add table below graph

--- data_visualization.py
+++ data_visualization.py
@@ -1,23 +1,33 @@
 import os
 import pandas as pd
-from dash import Dash, html, callback, Output, Input, dcc
+from dash import Dash, html, callback, Output, Input, dcc, dash_table
 import plotly.express as px
 
 DATA_FILEPATH = os.path.abspath('data/output.csv')
 
 df = pd.read_csv(DATA_FILEPATH)
 
-
 app = Dash()
-
 
 dropdown_values = df.Region.unique().tolist()
 dropdown_values.insert(0, 'all')
 
+
+app_title = html.H1(
+  children='Pink Morsel product sales',
+  style={
+    'textAlign': 'center',
+    'fontFamily': 'sans-serif'
+  })
+region_dropdown = dcc.Dropdown(dropdown_values, 'all', id="dropdown-select")
+graph = dcc.Graph(id="graph-content")
+data_table = dash_table.DataTable(data=df.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})
+
 app.layout = [
-  html.H1(children='Pink Morsel product sales', style={'textAlign': 'center'}),
-  dcc.Dropdown(dropdown_values, 'all', id="dropdown-select"),
-  dcc.Graph(id="graph-content")
+  app_title,
+  region_dropdown,
+  graph,
+  data_table
 ]
 
 @callback(
